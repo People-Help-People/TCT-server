ba112e5ede95b2a5499149138dbe76864177959e
HarishTeens <arishh2@gmail.com>
2022-01-23 15:15:41 +0530

    feat: fetch NFT balances from all chain

--- index.js
+++ index.js
@@ -39,6 +39,40 @@ app.get('/nft/verify', async (req, res) => {
     }
 });
 
+app.get('/nft/balance', async (req, res) => {
+    const address = req.query.address;
+    const chains = ["mainnet", "rinkeby", "ropsten", "kovan", "polygon", "mumbai"];
+
+    const nftBalances = await Promise.all(chains.map(async (chain) => {
+        // make RPC call to chain to get balance of address
+        const balanceResult = await fetch(`https://deep-index.moralis.io/api/v2/${address}/nft?chain=${chain}&format=hex`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'X-API-KEY': process.env.MORALIS_API_KEY
+            }
+        });
+        const balanceResultJson = await balanceResult.json();
+        const validNFTs = balanceResultJson.result.filter(nft => nft.is_valid).map(nft => {
+            return {
+                token_address: nft.token_address,
+                block_number: nft.block_number,
+                name: nft.name,
+                symbol: nft.symbol,
+                token_uri: nft.token_uri,
+                synced_at: nft.synced_at,
+                metadata: nft.metadata,
+            }
+         });
+        
+        return {
+            chain: chain,
+            balance: validNFTs
+        };
+    }));
+    res.json(nftBalances);
+});
+
 app.get('/twitter/verify', async (req, res) => {
     const tweet = req.query.tweet;
     const randid = req.query.randid;
@@ -78,5 +112,3 @@ if (process.env.PORT) {
         console.log('Server is running on port 4000');
     });
 }
-
-
